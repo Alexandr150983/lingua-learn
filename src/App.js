26c6e681e7a328281f4f136a6556cbe201c8746d
Alexandr150983 <alexandr150983@gmail.com>
2024-03-20 21:18:04 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import logo from "./logo.svg";
+import { Suspense } from "react";
 import "./App.css";
+import Layout from "./components/Layout/Layout";
+import Loader from "./components/Loader/Loader";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Teachers from "./pages/Teachers/Teachers";
+import Favorites from "./pages/Favorites/Favorites";
+import {
+  FAVORITES_ROUTE,
+  HOME_ROUTE,
+  TEACHERS_ROUTE,
+} from "./constants/routes";
 
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path={HOME_ROUTE} element={<Home />} />
+          <Route path={TEACHERS_ROUTE} element={<Teachers />} />
+          <Route path={FAVORITES_ROUTE} element={<Favorites />} />
+          <Route path="*" element={<Navigate replace to={HOME_ROUTE} />} />
+        </Routes>
+      </Suspense>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const HOME_ROUTE = "/home";
+export const FAVORITES_ROUTE = "/favorites";
+export const TEACHERS_ROUTE = "/teachers";
+export const REGISTER_ROUTE = "/signup";
+export const LOGIN_ROUTE = "/signin";
+export const LOGOUT_ROUTE = "/logout";
