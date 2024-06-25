59d785c56f130b443ec6654e85be07eb9fff2af8
Alexandr150983 <alexandr150983@gmail.com>
2024-06-25 14:28:33 +0300

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import "./services/firebase";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <PersistGate persistor={persistor} loading={null}>
           <App />
         </PersistGate>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
